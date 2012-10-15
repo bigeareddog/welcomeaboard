b043f5d280ffcb3a1383138af5eb109384d55d88
Tyson <tyson1704@gmail.com>
2012-10-16 07:35:08 +1000

    Add user Microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,7 +10,7 @@ module SessionsHelper
   end
 
   def current_user=(user)
-    @current_user = user
+     @current_user = user
   end
 
   def current_user
@@ -19,6 +19,13 @@ module SessionsHelper
 
   def current_user?(user)
     user == current_user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
   end
 
   def sign_out
